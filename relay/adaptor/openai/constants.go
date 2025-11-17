6a55db24f2cb862b7263ee7f35a30f8b1de1977b
lex <sunsan05@gmail.com>
2025-11-17 11:32:41 +0800

    add: gpt-5.1 series

--- relay/adaptor/openai/constants.go
+++ relay/adaptor/openai/constants.go
@@ -1,20 +1,20 @@
 package openai
 
 var ModelList = []string{
-	"o1",
-	"o1-mini",
-	"o3",
-	"o3-mini",
-	"o4-mini",
 	"whisper",
 	"tts",
 	"tts-hd",
-	"model-router",
 	"gpt-image-1",
+	"gpt-4.1",
+	"gpt-4.1-mini",
+	"gpt-4.1-nano",
 	"gpt-5",
 	"gpt-5-mini",
 	"gpt-5-nano",
 	"gpt-5-chat",
 	"gpt-5-codex",
 	"gpt-5-pro",
+	"gpt-5.1",
+	"gpt-5.1-codex",
+	"gpt-5.1-codex-mini",
 }
